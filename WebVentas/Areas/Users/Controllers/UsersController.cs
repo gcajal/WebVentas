5c7c14291f0f8e72a0babe985cbeb7c486b0c93d
gcajal19 <gabrielmcajal@gmail.com>
2020-04-08 21:09:57 -0300

    Agregar archivos de proyecto.

---
+++ WebVentas/Areas/Users/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebVentas.Areas.Users.Controllers
+{
+    [Area("Users")]//Especificamos la Area del controlador
+    public class UsersController : Controller
+    {
+        public IActionResult Users()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebVentas/Areas/Users/Models/InputModelRegister.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebVentas.Areas.Users.Models
+{
+    public class InputModelRegister
+    {
+        [Required(ErrorMessage ="El campo nombre es obligatorio")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "El campo apellido es obligatorio")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "El campo telefono es obligatorio")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{2})\)?[-.]?([0-9]{2})[-.]?([0-9]{5})$",ErrorMessage ="Formato invalido")]
+        public string PhoneNumber { get; set; }
+    }
+}
