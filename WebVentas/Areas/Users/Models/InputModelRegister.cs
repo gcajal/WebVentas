aa96ac58bb1d413f81bb78ea678e2e736f1177cf
gcajal19 <gabrielmcajal@gmail.com>
2020-04-08 21:15:06 -0300

    tes

--- WebVentas/Areas/Users/Models/InputModelRegister.cs
+++ WebVentas/Areas/Users/Models/InputModelRegister.cs
@@ -13,6 +13,7 @@ namespace WebVentas.Areas.Users.Models
         [Required(ErrorMessage = "El campo apellido es obligatorio")]
         public string LastName { get; set; }
 
+
         [Required(ErrorMessage = "El campo telefono es obligatorio")]
         [DataType(DataType.PhoneNumber)]
         [RegularExpression(@"^\(?([0-9]{2})\)?[-.]?([0-9]{2})[-.]?([0-9]{5})$",ErrorMessage ="Formato invalido")]
